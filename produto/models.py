c9f96130a41d332310dcce907cab0fde1ba65bb5
DanielAlmeida19 <dani1114vivilinda@gmail.com>
2025-02-05 14:45:25 -0300

    feat: create model Produto

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.CharField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(
+        upload_to='produto_imagens/%Y/%m/', blank=True, null=True
+    )
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default='v',
+        max_length=1,
+        choices=(
+            ('V', 'Variação'),
+            ('A', 'Simples')
+        )
+    )
+
+    def __str__(self):
+        return self.nome
+
+    @staticmethod
+    def resize_image(img, new_width=800):
+        print(img.name)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
