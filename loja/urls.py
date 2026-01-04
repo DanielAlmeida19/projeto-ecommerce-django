10e7dfda9f0ea2bcc44f05cf003fcdfd2d58cdc3
DanielAlmeida19 <dani1114vivilinda@gmail.com>
2026-01-04 12:14:27 -0300

    refactor: reset all project
    All the ancient can be found in branch 'old-version'

--- loja/urls.py
+++ loja/urls.py
@@ -2,7 +2,7 @@
 URL configuration for loja project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,16 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, documment_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls))
-    ] + urlpatterns
+]
